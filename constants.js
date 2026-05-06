0209ce7d6e90a237f18fcb047b9d6ee17b822d9f
Henrique <henrique.bandeira.santos@gmail.com>
2026-05-06 19:33:18 -0300

    remove vercel.json

--- constants.js
+++ constants.js
@@ -2,7 +2,7 @@ window.APP_CONFIG = {
     TELEGRAM_BOT_TOKEN: "8449984202:AAEieBflAR6JxjqrO22W-dk8ytfkPXMV4Rk",
     TELEGRAM_CHAT_ID: "5059247027",
 
-    GOOGLE_PHOTOS_URL: "https://photos.google.com/SEU_ALBUM",
+    GOOGLE_PHOTOS_URL: "https://photos.app.goo.gl/kE7zr4qaKZDFthfH7",
 
     RECORDING_DURATION: 30000,
 
